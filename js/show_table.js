fb147252d688b5563f571f06a71aa92ece00af0a
ebarns <erik.barns@yahoo.com>
2015-08-14 07:55:07 -0700

    resolving file path conflict

--- js/show_table.js
+++ js/show_table.js
@@ -3,12 +3,7 @@ $(document).ready(function() {
 
 	$('#show_table').click(function() {
 		// use AJAX to load load_table.php into .table_container div upon $show_table button press
-<<<<<<< HEAD
 		$('.table_container').load('../server/load_table.php');
-=======
-		$('.table_container').load('../happening/server/load_table.php');
->>>>>>> 03ba448b8f61193a32736568c040a06edb95b838
-		
 	});
 
 });
