bc22a8be7e5d68d9315ea3caaca6e43d062828a6
ebarns <erik.barns@yahoo.com>
2015-08-19 15:17:23 -0700

    knob layout work

--- index.php
+++ index.php
@@ -67,6 +67,23 @@ require 'init_sql.php';
                         $hours = floor($time_until / 3600) % 24;
 			$total_days = floor($events[$i][2] / 86400);
 			$days = ($time_until / 86400);
+              echo '<div class="row">
+		<div class="eventclock">
+                        <div class="dayz">
+                                <div class="innerd"><input id="day" type="text" value="' . $days . '"  data-min="0" data-max="' . $total_days . '" data-height="300" data-width="300" data-displayinput=false data-thickness="0.3" class="dial hour"></div>
+                        </div>
+                        <div class="hourz">
+                               <div class="innerh"> <input id="hour" type="text" value="' . $hours . '"  data-min="0" data-max="24" data-height="210" data-width="210" data-thickness="0.25" data-displayinput=false class="dial hour"></div>
+                        </div>
+                        <div class="minz">
+                                <div class="innerm"><input id="min" type="text" value="' . $min . '"  data-displayInput=false data-min="0" data-max="60" data-height="170" data-width="170" data-displayinput=false class="dial min"></div>
+                        </div>
+                        <div class="secz">
+                                <div class="inners"><input id="secs" type="text" value="' . $sec . '"  data-displayInput=false data-min="0" data-max="60" data-height="145" data-width="145" data-thickness="0.1" class="dial sec"></div>
+                        </div>
+                </div>
+		</div>';
+/*
 	                echo '<div class="row">
 				<div class="eventclock">
                                 <input id="day" type="text" value="' . $days . '"  data-min="0" data-max="' . $total_days . '" class="dial hour">
@@ -75,8 +92,61 @@ require 'init_sql.php';
                 		<input id="secs" type="text" value="' . $sec . '"  data-displayInput=false data-min="0" data-max="60" class="dial sec">
                 		</div>
                 	</div>';
+*/
 		}
         ?>
+<style>
+.row{
+	margin-top:300px;
+	margin-bottom:300px;
+}
+.eventclock{
+        position:relative;
+        width:350px;
+        margin:auto;
+}
+.secz{
+        position:absolute;
+        left:87.5px;
+        top:87px;
+}
+.inners{
+        display:inline;
+        width:100px;
+        height:100px;
+}
+.minz{
+        position:absolute;
+        left:75px;
+        top:75px;
+}
+.innerm{
+        display:inline;
+        width:100px;
+        height:100px;
+}
+.hourz{
+        position:absolute;
+        left:55px;
+        top:55px;
+}
+.innerh{
+        display:inline;
+        width:200px;
+        height:200px;
+}
+.dayz{
+        position:absolute;
+        left:10px;
+        top:10px;
+}
+
+.innerd{
+        display:inline;
+        width:300px;
+        height:300px;
+}
+</style>
 
 	<script>create_knob();</script>
 	<script>
